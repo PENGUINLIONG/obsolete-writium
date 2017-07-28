6ad114d1fbd94160ebd8ba43219d847b5ee6d9f1
PENGUINLIONG <penguinliong@sina.com>
2017-07-27 23:29:01 -0700

    Clean up

--- src/writus/mod.rs
+++ src/writus/mod.rs
@@ -1,100 +1,15 @@
 extern crate markdown;
 extern crate iron;
 
-use std::fs::{File};
-use std::io::{Read};
-use std::path::Path;
-
 use self::iron::prelude::*;
 use self::iron::method::Method;
 use self::iron::status;
 
+mod resource;
 mod response_gen;
 mod settings;
 
 use self::response_gen::{gen_error, gen_error_page, gen_page, gen_spec};
-
-/// Find resource from local storage.
-///
-/// Some(~) will be returned if the requested resource is successfully read.
-/// None, otherwise.
-fn find_rsc(local_dir: &str, path: &str) -> Option<Vec<u8>> {
-    let local_path = local_dir.to_owned() + path;
-    println!("Looking for file in local storage: {}", local_path);
-    let local_path = Path::new(&local_path);
-    if let Ok(mut file) = File::open(&local_path) {
-        // Fetch content.
-        let mut content = Vec::<u8>::new();
-        if file.read_to_end(&mut content).is_err() {
-            return None;
-        }
-        return Some(content);
-    } else {
-        return None;
-    }
-}
-
-fn make_response_rsc(local_dir: &str, rel_path: &str, ext: &str) -> Response {
-    /// Map file extension to media type.
-    fn map_ext(ext: &str) -> Option<&str> {
-        match ext {
-            // General.
-            "htm" => Some("text/html"),
-            "html" => Some("text/html"),
-            "js" => Some("application/javascript"),
-            "css" => Some("text/css"),
-            // Image.
-            "jpg" => Some("image/jpeg"),
-            "jpeg" => Some("image/jpeg"),
-            "png" => Some("image/png"),
-            "gif" => Some("image/gif"),
-            _ => None,
-        }
-    }
-    match map_ext(ext) {
-        Some(content_type) => match find_rsc(&local_dir, &rel_path) {
-            Some(rsc) => gen_spec(rsc, content_type.to_string()),
-            None => {
-                println!("Resource not found.");
-                gen_error(status::NotFound)
-            },
-        },
-        None => {
-            println!("Unrecognized file extension.");
-            gen_error(status::NotFound)
-        },
-    }
-}
-
-fn make_response_article(local_dir: &str, rel_path: &str) -> Response {
-    /// Translate markdown byte sequence to HTML.
-    fn translate_article(bytes: Vec<u8>) -> Option<String> {
-        match String::from_utf8(bytes) {
-            Ok(content) => {
-                println!("Translate Markdown into HTML.");
-                Some(markdown::to_html(&content))
-            },
-            Err(_) => None,
-        }
-    }
-    let rel_path = rel_path.to_owned() + ".md";
-    // Not found, it should be an article.
-    match find_rsc(&local_dir, &rel_path) {
-        // Article present, translate markdown to HTML.
-        Some(content) => match translate_article(content) {
-            Some(html) => gen_page(html),
-            // Treat translation error as file-not-found error.
-            None => {
-                println!("Markdown translation failed.");
-                gen_error_page(status::NotFound)
-            },
-        },
-        None => {
-            println!("Article absent.");
-            gen_error_page(status::NotFound)
-        },
-    }
-}
 
 /// Response to incoming requests.
 fn make_response(req: &Request) -> Response {
@@ -130,17 +45,14 @@ fn make_response(req: &Request) -> Response {
         },
     };
     // Read data from storage.
-    // The last section of $path contains the file name and its extension, which
-    // indicates its file format.
-    let rel_path = "/".to_owned() + &path[1..].join("/");
-    if let Some(ext_pos) = rel_path.rfind(".") {
-        // '.' was found, there is an extension. Look up supported file formats.
-        make_response_rsc(&local_dir, &rel_path, &rel_path[(ext_pos + 1)..])
-    } else if search_dir == "post" {
-        make_response_article(&local_dir, &rel_path)
-    } else {
-        println!("Article can only be in `./post`.");
-        gen_error_page(status::NotFound)
+    use self::resource::Resource::{Article, Material};
+    let local_path = local_dir + &"/" + &path[1..].join("/");
+    match resource::get_resource(&local_path, search_dir == "post") {
+        Some(rsc) => match rsc {
+            Article { content } => gen_page(markdown::to_html(&content)),
+            Material { media_type, data } => gen_spec(data, media_type),
+        },
+        None => gen_error_page(status::NotFound),
     }
 }
 
---
+++ src/writus/resource.rs
@@ -0,0 +1,85 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+pub enum Resource {
+    Material {
+        media_type: String,
+        data: Vec<u8>,
+    },
+    Article {
+        content: String
+    },
+}
+
+/// Load resource from local storage.
+///
+/// Some(~) will be returned if the requested resource is successfully read.
+/// None, otherwise.
+fn load_resource(local_path: &str) -> Option<Vec<u8>> {
+    println!("Looking for file in local storage: {}", local_path);
+    let local_path = Path::new(&local_path);
+    if let Ok(mut file) = File::open(&local_path) {
+        // Fetch content.
+        let mut content = Vec::<u8>::new();
+        match file.read_to_end(&mut content) {
+            Ok(_) => Some(content),
+            Err(_) => None,
+        }
+    } else {
+        None
+    }
+}
+
+/// Get file extension in path. None is returned if there isn't one.
+fn deduct_type_by_ext(local_path: &str) -> Option<&str> {
+    /// Map file extension to media type.
+    fn map_ext(ext: &str) -> Option<&str> {
+        match ext {
+            // General.
+            "htm" => Some("text/html"),
+            "html" => Some("text/html"),
+            "js" => Some("application/javascript"),
+            "css" => Some("text/css"),
+            // Image.
+            "jpg" => Some("image/jpeg"),
+            "jpeg" => Some("image/jpeg"),
+            "png" => Some("image/png"),
+            "gif" => Some("image/gif"),
+            _ => None,
+        }
+    }
+    match local_path.rfind(".") {
+        Some(ext_pos) => map_ext(&local_path[(ext_pos + 1)..]),
+        None => None,
+    }
+}
+
+/// Get resource file.
+pub fn get_resource(local_path: &str, in_post: bool) -> Option<Resource> {
+    use self::Resource::{Article, Material};
+    match deduct_type_by_ext(&local_path) {
+        // Extension present, return material.
+        Some(media_type) => match load_resource(local_path) {
+            Some(data) => Some(Material {
+                media_type: media_type.to_owned(),
+                data: data,
+            }),
+            None => None,
+        },
+        // Extension absent, return article.
+        None => if in_post { // Article can only be in `./post`.
+            match load_resource(&(local_path.to_owned() + ".md")) {
+                Some(data) => match String::from_utf8(data) {
+                    Ok(content) => Some(Article {
+                        content: content,
+                    }),
+                    Err(_) => None,
+                },
+                None => None,
+            }
+        } else {
+            None
+        }
+    }
+}
